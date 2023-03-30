aa3aae27a275c30b28a073af08fe107edf658cbb
DoChuan13 <chuando.9213@gmail.com>
2023-03-30 17:33:04 +0700

    Account Validate

---
+++ src/AccountExampleTest.java
@@ -0,0 +1,17 @@
+public class AccountExampleTest {
+    public static final String[] validAccount = new String[]{"123abc_", "_abc123", "______", "123456", "abcdefgh"};
+    public static final String[] invalidAccount = new String[]{".@", "12345", "1234_", "abcde"};
+    private static AccountExample accountExample;
+
+    public static void main(String args[]) {
+        accountExample = new AccountExample();
+        for (String account : validAccount) {
+            boolean isvalid = accountExample.validate(account);
+            System.out.println("Account is " + account + " is valid: " + isvalid);
+        }
+        for (String account : invalidAccount) {
+            boolean isvalid = accountExample.validate(account);
+            System.out.println("Account is " + account + " is valid: " + isvalid);
+        }
+    }
+}
